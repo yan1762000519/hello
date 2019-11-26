3090395df699a8aab6f08391ab709a8898cc56ef
yxy <1762000519@qq.com>
2019-11-26 14:55:58 +0800

    静态库实现与调用

---
+++ staticlib/MathFuncsLib/MathFuncsLib/MathFuncsLib.cpp
@@ -0,0 +1,32 @@
+﻿// MathFuncsLib.cpp : 定义静态库的函数。
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "MathFuncsLib.h"
+#include <stdexcept>
+
+using namespace std;
+
+namespace MathFuncs
+{
+	double MyMathFuncs::Add(double a, double b)
+	{
+		return a + b;
+	}
+
+	double MyMathFuncs::Subtract(double a, double b)
+	{
+		return a - b;
+	}
+
+	double MyMathFuncs::Multiply(double a, double b)
+	{
+		return a * b;
+	}
+
+	double MyMathFuncs::Divide(double a, double b)
+	{
+		return a / b;
+	}
+}
